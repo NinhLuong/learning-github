90a252b9e31505d8f44c749b0bc5b48b80b56a5a
Luong Ninh <luongvuhaininh123@gmail.com>
2023-06-26 16:44:23 +0700

    change file old_file

--- old_file.py
+++ old_file.py
@@ -15,8 +15,8 @@ def replace_domain(address, old_domain, new_domain):
 def main():
   """Processes the list of emails, replacing any instances of the old domain with the new domain."""
   old_domain, new_domain = 'abc.edu', 'xyz.edu'
-  csv_file_location = "D:/Linux/it_automation/course3/user_emails.csv" #path csv_file_location
-  report_file = "D:/Linux/it_automation/course3/updated_user_emails.csv" #path report_file
+  csv_file_location = '<csv_file_location>'
+  report_file = '<path_to_home_directory>' + '/updated_user_emails.csv' #path report file location
   user_email_list = []
   old_domain_email_list = []
   new_domain_email_list = []
@@ -40,3 +40,4 @@ def main():
     writer.writerows(user_data_list)
     output_file.close()
 main()
+# end code 
